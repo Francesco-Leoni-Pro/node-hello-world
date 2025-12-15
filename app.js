2138f94095ec5ae51341fb79a8a03ece59544d63
Francesco-Leoni-Pro <francesco.leoni.pro@icloud.com>
2025-12-15 16:44:35 +0100

    feat: chalk

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log("Hello World!");
 const argomento = process.argv[2];
 
 console.log("Hello " + argomento);
+
+import chalk from 'chalk';
+
+const argomento2 = process.argv[2];
+
+console.log(chalk.green('Hello') + ' ' + chalk.bgBlue(argomento2));
